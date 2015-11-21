3bddd18cc9ef7db5b1c8f989cc076a27c828dee8
Mihai Varga <mihai.mv13@gmail.com>
2015-11-21 16:42:16 +0200

    server: register and notify endpoints

--- server/main.py
+++ server/main.py
@@ -1,3 +1,7 @@
+import cgi
+import json
+import urllib
+import urllib2
 import webapp2
 from random import choice
 from google.appengine.ext import db
@@ -22,6 +26,29 @@ class MainPage(webapp2.RequestHandler):
         for u in registered_users:
           self.response.write(str(u)+'\n')
 
+class Register(webapp2.RequestHandler):
+    def post(self):
+        self.response.write(cgi.escape(self.request.get('gcmtoken')))
+        self.response.write(cgi.escape(self.request.get('name')))
+
+class Notifier(webapp2.RequestHandler):
+    def post(self):
+        url = 'http://gcm-http.googleapis.com/gcm/send'
+        to = str(cgi.escape(self.request.get('to')))
+        message = str(cgi.escape(self.request.get('message')))
+        data = {
+                "data": {'message': message},
+                "to": to
+                }
+        headers = {
+                "Content-Type":"application/json",
+                "Authorization":"key=AIzaSyBk3-v9AaKz8s2KYLuImlsIBSl1GF6XGlM"
+                }
+        req = urllib2.Request(url, json.dumps(data), headers)
+        response = urllib2.urlopen(req)
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/register', Register),
+    ('/notify', Notifier),
 ], debug=True)
