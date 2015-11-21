47a6b018e54b9e6d839d7edaad6c0a52563f3f7b
Alexandru Tifrea <alex.tifrea93@gmail.com>
2015-11-21 16:43:49 +0200

    Added endpoints

--- server/main.py
+++ server/main.py
@@ -1,27 +1,36 @@
 import webapp2
 from random import choice
 from google.appengine.ext import db
-from db_proba import User
+import json
+from RegisteredUser import RegisteredUser
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!\n')
-        u = User(
-          name=choice(["John", "Mihaitza", "Bogdanel"]),
-          location = db.GeoPt(lat=45.0, lon=25.0),
-          rank=0,
-          role="default"
+class RegisterUser(webapp2.RequestHandler):
+    def post(self):
+        u = RegisteredUser(
+            name=choice(["John", "Mihaitza", "Bogdanel"]),
+            location = db.GeoPt(lat=45.0, lon=25.0),
+            rank=0,
+            role="default"
         )
         u.put()
 
+class GetHelp(webapp2.RequestHandler):
+    def post(self):
+        print "CORPUL MEU",self.request.body
+        self.response.headers['Content-Type'] = 'text/plain'
         registered_users = db.GqlQuery(
-          "SELECT * FROM User WHERE rank = 0"
+            "SELECT * FROM RegisteredUser WHERE rank = 0"
         )
         self.response.write("Registered users:\n")
         for u in registered_users:
-          self.response.write(str(u)+'\n')
+            self.response.write(str(u)+'\n')
+
+class UpdateUserData(webapp2.RequestHandler):
+    def post(self):
+        pass
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/registeruser', RegisterUser),
+    ('/gethelp', GetHelp),
+    ('/updateuserdata', UpdateUserData),
 ], debug=True)
